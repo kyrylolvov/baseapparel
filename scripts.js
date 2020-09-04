83cf1809b3990297b55535a378c538cac5c27a6a
Kyrylo Lvov <70720713+kyrylolvov@users.noreply.github.com>
2020-09-04 23:36:14 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('invalid', (function(){
+    return function(e) {
+      //prevent the browser from showing default error bubble / hint
+      e.preventDefault();
+      // optionally fire off some custom validation handler
+      // myValidation();
+    };
+})(), true);
